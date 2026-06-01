d82d03b0e1ed912257830e59abd2c1f7dcfdf66f
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2026-06-01 12:19:39 +0800

    Refactor to allow settings to be reloaded

--- settings/settings.py
+++ settings/settings.py
@@ -4,28 +4,37 @@ from settings.default import default_settings
 
 
 class Settings:
-    def __init__(self, file_path):
+    def __init__(self, file_path: str) -> None:
         self.file_path = file_path
+        self.load_settings()
 
+    def load_settings(self) -> None:
+        self.data = self._load_settings_from_file()
+        self._parse_settings(self, self.data)
+
+    def _load_settings_from_file(self) -> dict:
         try:
-            with open(file_path, "r", encoding="utf-8") as f:
+            with open(self.file_path, "r", encoding="utf-8") as f:
                 data = json.load(f)
         except FileNotFoundError:
             data = default_settings
 
-            with open(file_path, "w", encoding="utf-8") as f:
+            with open(self.file_path, "w", encoding="utf-8") as f:
                 json.dump(data, f, indent=2)
 
-        self._load_dict(self, data)
+        return data
 
-    def _load_dict(self, obj, data):
+    def _parse_settings(self, obj, data: dict) -> None:
         for key, value in data.items():
             if isinstance(value, dict):
                 nested_obj = SimpleNamespace()
                 setattr(obj, key, nested_obj)
-                self._load_dict(nested_obj, value)
+                self._parse_settings(nested_obj, value)
             else:
                 setattr(obj, key, value)
+
+    def reload_settings(self) -> None:
+        self.load_settings()
 
 
 settings = Settings("settings.json")
