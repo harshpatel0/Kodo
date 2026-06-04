4cdbef09b98c6d82c0e865e5f84c34a0f5e64108
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2026-06-04 21:39:48 +0800

    Browser Skill now tries to load in a new window instead

--- skills/browser-navigation/skill.py
+++ skills/browser-navigation/skill.py
@@ -2,9 +2,26 @@ import webbrowser
 import sys
 import json
 
+# Set Opening Mode to existing, tab, or window to open the url in a existing browser window, a tab in the existing window, or just a new window
+OPEN_MODE = "window"
 
-def open_url(url):
-    webbrowser.open(url=url)
+
+def open_url(url: str) -> None:
+    webbrowser_opening_mode = 1
+
+    url = url.strip()
+
+    if OPEN_MODE == "existing":
+        webbrowser_opening_mode = 0
+    if OPEN_MODE == "tab":
+        webbrowser_opening_mode = 2
+    if OPEN_MODE == "window":
+        webbrowser_opening_mode = 1
+
+    if not url.lower().startswith(("http://", "https://")):
+        url = "http://" + url
+
+    webbrowser.open(url=url, new=webbrowser_opening_mode)
 
 
 if __name__ == "__main__":
