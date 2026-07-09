d72c9e80410ad4f0eb778df864c6ee3e264e7c6d
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2026-07-09 23:18:54 +0800

    Remove unused file

--- utils/toasts/toast_decorators.py
+++
@@ -1,6 +0,0 @@
-from utils import toaster
-
-
-def skill_installation_toast_decorator(function):
-    def wrapper(*args, **kwargs):
-        toaster.update("Skill Installation Mode", "Installing Skills")
